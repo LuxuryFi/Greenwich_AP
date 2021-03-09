50d48ffec9e57a614c9e2409c2efcab823d1d007
Nguyễn Quốc Anh <65543110+LuxuryFi@users.noreply.github.com>
2021-03-10 00:48:50 +0700

    Update Database

--- example.js
+++ example.js
@@ -11,11 +11,7 @@ const app = express();
 
 var getListStudent = require('./getListStudent');
 
-axios.defaults.headers.common['Cookie'] = 'G_ENABLED_IDPS=google; _ga=GA1.3.358351611.1615029154; ASP.NET_SessionId=4cfcvr1qp3fxpnrpagnfbqgq; G_AUTHUSER_H=1';
-
- 
-
-
+// axios.defaults.headers.common['Cookie'] = 'G_ENABLED_IDPS=google; _ga=GA1.3.358351611.1615029154; ASP.NET_SessionId=4cfcvr1qp3fxpnrpagnfbqgq; G_AUTHUSER_H=1';
 
 var con = mysql.createConnection({
     host: "45.124.94.41",
@@ -25,21 +21,45 @@ var con = mysql.createConnection({
 });
 
 
-app.get('/getListStudent',async (res,req,next) => {
+app.get('/getListStudent', async (req, res, next) => {
     let list = await getListStudent.getStudent;
-    console.log(list);
+    list.forEach(element => {
+        var sql = "INSERT trainee (trainee_firstname,trainee_lastname,trainee_email,avatar) VALUES ('"+element.firstname+"','" +element.lastname+"','"+element.email+"','"+element.avatar+"')";
+        // con.query(sql, function (err, result) {
+        // if (err) throw err;
+        // console.log("1 record inserted");
+        // });
+        // console.log(sql)
+
+    });
+    res.send(list);
 })
 
-app.get('/getListTopic',async (res,req,next) => {
+app.get('/getListTopic', async (req, res, next) => {
     let list = await getListStudent.getTopic;
-    console.log(list);
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        list.forEach(element => {
+            var sql = "INSERT INTO topic (topic_name,credit,topic_code,semester) VALUES ('" +element.topic_name+"','" +element.credit+"','"+element.topic_code+"','"+element.semester+"')";
+            // con.query(sql, function (err, result) {
+            // if (err) throw err;
+            // console.log("1 record inserted");
+            // });
+            console.log(sql)
+
+        });
+       
+      });
+
+
+    res.send(list);
 })
 
 
 
-app.listen(3000,function(){
+
+app.listen(3001, function () {
     console.log('Node server running @ http://localhost:3000')
 });
-
-
-
--- getListStudent.js
+++ getListStudent.js
@@ -1,6 +1,3 @@
-
-
-
 const axios = require('axios');
 const fs = require('fs');
 const jsdom = require('jsdom');
@@ -9,9 +6,9 @@ const {
 } = jsdom;
 const url = require('url');
 
-axios.defaults.headers.common['Cookie'] = 'G_ENABLED_IDPS=google; _ga=GA1.3.358351611.1615029154; ASP.NET_SessionId=4cfcvr1qp3fxpnrpagnfbqgq; G_AUTHUSER_H=1';
+axios.defaults.headers.common['Cookie'] = 'G_ENABLED_IDPS=google; _ga=GA1.3.358351611.1615029154; ASP.NET_SessionId=kplkn402q4sqjxvsoumxib1e; G_AUTHUSER_H=1';
 
- 
+
 
 
 
@@ -25,19 +22,23 @@ async function getStudent() {
     for (const tr of dom.querySelectorAll('#ctl00_mainContent_divStudents tbody tr')) {
         students.push({
             avatar: new URL(tr.children.item(1).querySelector('img').src, 'https://ap.greenwich.edu.vn/Course').href,
-            username: tr.children.item(2).textContent.trim(),
+            email: tr.children.item(2).textContent.trim(),
             code: tr.children.item(3).textContent.trim(),
+            firstname: tr.children.item(4).textContent.trim(),
+            lastname: tr.children.item(6).textContent.trim()
         });
-       
-    }
+
+    }   
+
+    console.log(students)
 
     for (const student of students) {
         const imgData = await axios.get(student.avatar, {
             responseType: 'arraybuffer'
         });
 
-        fs.writeFileSync('./images/' + student.username + '.jpg', imgData.data);
-        student.avatar = student.username + '.jpg'
+        fs.writeFileSync('G:/test-exp/public/uploads/trainees/' + student.email + '.jpg', imgData.data);
+        student.avatar = student.email + '.jpg'
     }
 
     return await students;
@@ -50,17 +51,18 @@ async function getTopic() {
     const dom = new JSDOM(res.data).window.document;
 
     var topics = [];
- 
-    for (const tr of dom.querySelectorAll('#ctl00_mainContent_divGrade tbody tr')) {
+
+    for (const tr of dom.querySelectorAll('#ctl00_mainContent_divGrade > table:first-child tbody tr')) {
         topics.push({
-            //  topic_code: tr.children.item(4).textContent.trim(),
-            // topic_name: tr.children.item(6).textContent.trim(),
-            // credit: tr.children.item(7).textContent.trim(),
-            // semester: tr.children.item(3).textContent.trim()
+            topic_code: tr.children.item(3).textContent.trim(),
+            topic_name: tr.children.item(5).textContent.trim(),
+            credit: 15,
+            semester: tr.children.item(3).textContent.trim(),
+            slot: 40,
+            is_active: 1
         });
     }
-    console.log(res.data)
-   return await topics;
+    return topics;
 }
 
 module.exports.getStudent = getStudent();
